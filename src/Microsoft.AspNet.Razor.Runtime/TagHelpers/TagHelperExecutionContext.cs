3a4220532a6563df2a2209b1c5857f732ebd78ed
N. Taylor Mullen <nimullen@microsoft.com>
2014-12-01 15:39:29 -0800

    Re-design TagHelperOutput and runtime dependencies to allow all content modes.
    - Added PreContent, PostContent and ContentSet properties to TagHelperOutput.
    - Added GeneratePreContent, GeneratePostContent and SupressOutput methods to TagHelperOutput.
    - Added multile ExecuteChildContentAsync and GetChildContentAsync to the rendering phase, ultimately only exposing GetChildContentAsync to a TagHelper author.
    - Added more knowledge of StartWritingScope and EndWritingScope to the TagHelper runtime components. This is to enable the runtime components to utilize the RazorPage's infrastructure to render a delegate to a writer and retrieve its value to ultimately expose it to the user.
    
    #221

--- src/Microsoft.AspNet.Razor.Runtime/TagHelpers/TagHelperExecutionContext.cs
+++ src/Microsoft.AspNet.Razor.Runtime/TagHelpers/TagHelperExecutionContext.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Microsoft.AspNet.Razor.Runtime.TagHelpers
 {
@@ -12,13 +14,34 @@ namespace Microsoft.AspNet.Razor.Runtime.TagHelpers
     public class TagHelperExecutionContext
     {
         private readonly List<ITagHelper> _tagHelpers;
+        private string _childContent;
 
         /// <summary>
         /// Instantiates a new <see cref="TagHelperExecutionContext"/>.
         /// </summary>
         /// <param name="tagName">The HTML tag name in the Razor source.</param>
-        public TagHelperExecutionContext([NotNull] string tagName, [NotNull] string uniqueId)
+        /// <param name="uniqueId">An identifier unique to the HTML element this context is for.</param>
+        /// <param name="executeChildContentAsync">A delegate used to execute the child content asynchronously.</param>
+        /// <param name="startWritingScope">A delegate used to start a writing scope in a Razor page.</param>
+        /// <param name="endWritingScope">A delegate used to end a writing scope in a Razor page.</param>
+        public TagHelperExecutionContext([NotNull] string tagName,
+				         [NotNull] string uniqueId,
+                                         [NotNull] Func<Task> executeChildContentAsync,
+                                         [NotNull] Action startWritingScope,
+                                         [NotNull] Func<TextWriter> endWritingScope)
         {
+            ExecuteChildContentAsync = executeChildContentAsync;
+            GetChildContentAsync = async () =>
+            {
+                if (_childContent == null)
+                {
+                    startWritingScope();
+                    await executeChildContentAsync();
+                    _childContent = endWritingScope().ToString();
+                }
+
+                return _childContent;
+            };
             AllAttributes = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
             HTMLAttributes = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
             _tagHelpers = new List<ITagHelper>();
@@ -36,19 +59,43 @@ namespace Microsoft.AspNet.Razor.Runtime.TagHelpers
         }
 
         /// <summary>
+        /// A delegate used to execute the child content asynchronously.
+        /// </summary>
+        public Func<Task> ExecuteChildContentAsync { get; }
+
+        /// <summary>
+        /// A delegate used to execute and retrieve the rendered child content asynchronously.
+        /// </summary>
+        /// <remarks>
+        /// Child content is only executed once. Successive calls to this delegate return a cached result.
+        /// </remarks>
+        public Func<Task<string>> GetChildContentAsync { get; }
+
+        /// <summary>
+        /// Indicates if <see cref="GetChildContentAsync"/> has been called.
+        /// </summary>
+        public bool ChildContentRetrieved
+        {
+            get
+            {
+                return _childContent != null;
+            }
+        }
+
+        /// <summary>
         /// HTML attributes.
         /// </summary>
-        public IDictionary<string, string> HTMLAttributes { get; private set; }
+        public IDictionary<string, string> HTMLAttributes { get; }
 
         /// <summary>
         /// <see cref="ITagHelper"/> bound attributes and HTML attributes.
         /// </summary>
-        public IDictionary<string, object> AllAttributes { get; private set; }
+        public IDictionary<string, object> AllAttributes { get; }
 
         /// <summary>
         /// An identifier unique to the HTML element this context is for.
         /// </summary>
-        public string UniqueId { get; private set; }
+        public string UniqueId { get; }
 
         /// <summary>
         /// <see cref="ITagHelper"/>s that should be run.
@@ -64,7 +111,7 @@ namespace Microsoft.AspNet.Razor.Runtime.TagHelpers
         /// <summary>
         /// The HTML tag name in the Razor source.
         /// </summary>
-        public string TagName { get; private set; }
+        public string TagName { get; }
 
         /// <summary>
         /// The <see cref="ITagHelper"/>s' output.
